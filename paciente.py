7b44523c76e3c960e0473694d51168d03f2f69c3
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-06-22 12:34:40 -0300

    Update paciente.py

--- paciente.py
+++ paciente.py
@@ -1,22 +1,15 @@
 from pessoa import Pessoa 
 
 class Paciente(Pessoa):
-    def __init__(self, nome, idade, cpf, convenio):
-        super().__init__(nome,idade)
+    def __init__(self, nome, senha, cpf):
+        super().__init__(nome,senha)
         self.cpf = cpf
-        self.convenio = convenio
     
     def get_cpf(self):
         return self.cpf
     
     def set_cpf(self, cpf):
         self.cpf = cpf
-
-    def get_convenio(self):
-        return self.convenio
-    
-    def set_convenio(self, convenio):
-        self.convenio = convenio
         
     def descricao(self):
-        return f"{super().descricao()}, CPF: {self.cpf}, Sintomas: {self.sintomas}, Convênio: {self.convenio}"
+        return f"{super().descricao()}, CPF: {self.cpf}, "
