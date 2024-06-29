4fb03451af4ceba73f350f4a38ad02b591aefbac
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-06-29 15:29:58 -0300

    Create consulta.py

---
+++ consulta.py
@@ -0,0 +1,6 @@
+class Consulta:
+    def __init__(self, id, paciente, medico, horario):
+        self.id = id
+        self.paciente = paciente
+        self.medico = medico
+        self.horario = horario
