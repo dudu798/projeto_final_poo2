076db97ede99ef7506fecbc60c9f3fa99f2493ef
joaomdaltoe <152723667+joaomdaltoe@users.noreply.github.com>
2024-06-29 15:30:20 -0300

    Update medico.py

--- medico.py
+++ medico.py
@@ -1,16 +1,19 @@
 from pessoa import Pessoa
 
 class Medico(Pessoa):
-    def __init__(self, nome, senha, crm, especializacao):
+    def __init__(self, nome, senha, CRM, Especialização):
         super().__init__(nome, senha)
-        self.crm = crm
-        self.especializacao = especializacao
+        self.crm = CRM
+        self.especializacao = Especialização
 
     def get_crm(self):
         return self.crm
     
     def set_crm(self, crm):
         self.crm = crm
+
+    def get_nome(self):
+        return self.nome
 
     def get_especializacao(self):
         return self.especializacao
@@ -19,4 +22,4 @@ class Medico(Pessoa):
         self.especializacao = especializacao
         
     def descricao(self):
-        return f"{super().descricao()}, CRM: {self.crm}, Especialização: {self.especializacao}"
+        return {'nome': self.nome, 'senha': self.senha_codificada, "CRM": self.crm, "Especialização": self.especializacao}
